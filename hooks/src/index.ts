343d527b2b236e55861815a57a3d4f6fa8066a3e
saketsingh0078 <saketkrsingh0078@gmail.com>
2024-10-29 22:08:05 +0530

    added prisma and write schema

--- hooks/src/index.ts
+++ hooks/src/index.ts
@@ -1,14 +1,23 @@
-import express from "express"
+import express from "express";
+import { PrismaClient } from "@prisma/client";
 
-const app=  express ( ); 
+const app = express();
+const client = new PrismaClient();
 
+app.get("/", (req, res) => {});
+// some password logic
+app.post("/hooks/catch/:userId/:zapId", (req, res) => {
+  const userId = req.params.userId;
+  const zapId = req.params.zapId;
 
-app.get("/",(req,res)=>{
-  
-}) {
+  // store in a new trigger
+  await client.zapRun.create({
+    data: {},
+  });
 
-}
+  // push it on to a kafka / redis
+});
 
-app.listen("3000",()=>{
-  console.log("Server connected")
-})
+app.listen("3000", () => {
+  console.log("Server connected");
+});
