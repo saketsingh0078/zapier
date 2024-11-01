b60c55b14cefceda35574dbcde174aee2f86691b
saketsingh0078 <saketkrsingh0078@gmail.com>
2024-11-01 14:19:47 +0530

    added outProcessor

--- outBoxProcessor/src/index.ts
+++ outBoxProcessor/src/index.ts
@@ -2,6 +2,7 @@ import { Kafka } from "kafkajs";
 import { PrismaClient } from "@prisma/client";
 
 const client = new PrismaClient();
+const TOPIC_NAME = "zap-events";
 
 const kafka = new Kafka({
   clientId: "outBoxProcessor",
@@ -11,16 +12,25 @@ const kafka = new Kafka({
 async function main() {
   const consumer = kafka.consumer({ groupId: "test-group" });
   await consumer.connect();
-  await consumer.subscribe({ topic: "zap-events", fromBeginning: true });
+  await consumer.subscribe({ topic: TOPIC_NAME, fromBeginning: true });
 
   while (1) {
     await consumer.run({
+      autoCommit: false,
       eachMessage: async ({ topic, partition, message }) => {
         console.log({
           partition,
           offset: message.offset,
           value: message.value.toString(),
         });
+        await new Promise((r) => setTimeout(r, 5000));
+        await consumer.commitOffsets([
+          {
+            topic: TOPIC_NAME,
+            partition: partition,
+            offset: (parseInt(message.offset) + 1).toString(),
+          },
+        ]);
       },
     });
   }
