341bd1f433d1d3fdb6d1d6074a76f9eeed3de144
saketsingh0078 <saketkrsingh0078@gmail.com>
2024-11-27 18:12:34 +0530

    database init

--- hooks/src/index.ts
+++ hooks/src/index.ts
@@ -1,44 +1,12 @@
 import express from "express";
-import { PrismaClient } from "@prisma/client";
 
 const app = express();
-const client = new PrismaClient();
 
-app.get("/", (req, res) => {
-  res.json({
-    msg: " Hello ",
-  });
-});
-
-// some password logic
-app.post("/hooks/catch/:userId/:zapId", async (req, res) => {
+app.post("/hooks/catch/:userId/:zapId", (req, res) => {
   const userId = req.params.userId;
   const zapId = req.params.zapId;
-  const body = req.body;
 
-  // store in a new trigger
-  await client.$transaction(async (tx) => {
-    const run = await client.zapRun.create({
-      data: {
-        zapId,
-        metaData: body,
-      },
-    });
+  //store in the db a new trigger
 
-    await client.zapRunOutBox.create({
-      data: {
-        zapRunId: run.id,
-      },
-    });
-  });
-
-  res.json({
-    msg: "webhook received",
-  });
-
-  // push it on to a kafka / redis
-});
-
-app.listen("3000", () => {
-  console.log("Server connected");
+  //push it on to a queue (kafka/redis)
 });
